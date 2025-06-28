dc5aac200000566725a8fb9f18c4198a11523903
phani19s <142476869+phani19s@users.noreply.github.com>
2025-06-28 11:29:38 +0530

    Add files via upload

---
+++ funtions.py
@@ -0,0 +1,13 @@
+'''#user defined function
+
+def add():
+    a=int(input())
+    b=int(input())
+    return a+b
+print(add())
+'''
+#passing parameters
+
+def multiply(a,b):
+    return a*b
+print(multiply(5,2))
