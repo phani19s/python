aba48f4c7ebd8dda4058d9d495ab7ac097230588
phani19s <142476869+phani19s@users.noreply.github.com>
2025-06-30 18:33:57 +0530

    Update arthematic_operators.py

--- arthematic_operators.py
+++ arthematic_operators.py
@@ -1,44 +1,4 @@
 '''
-#Area of square
-
-s=int(input())
-area=s*s
-print("Area of the square is:",area)
-'''
-'''
-#Area of rectangle
-
-l=int(input())
-b=int(input())
-area=l*b
-print("Area :",area)
-''''''
-#Area of triangle
-
-b=int(input())
-h=int(input())
-area=0.5*h*b
-print("Area :",area)
-''''''
-#perimeter of square
-
-s=int(input())
-pe=4*s
-print("Perimeter :",pe)
-''''''
-#perimeter of retangle
-
-l=int(input())
-b=int(input())
-p=2*(l+b)
-print("Perimeter :",p)
-''''''
-#perimeter of triangle
-
-a,b,c=map(int,input().split())
-p=a+b+c
-print("Perimeter :",p)
-''''''
 #Denomination
 
 N=int(input())
@@ -61,19 +21,6 @@ print("Hours :",h)
 print("Minutes :",m)
 print("Seconds :",t)
 ''''''
-#sum of marks(maths,physics,chemistry)
-
-m=int(input())
-p=int(input())
-c=int(input())
-print("Sum :",(m+p+c))
-#Average of marks
-print("Average :",((m+p+c)/3))
-'''
-
-
-
-
 
 
 
